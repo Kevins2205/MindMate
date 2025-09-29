80b0a65fe2301d0a2f9bde2454ef29f66840ffa0
Kevins2205 <kevin220503@gmail.com>
2025-09-29 17:25:32 +0200

    add_homepage

--- app/src/main/java/com/example/mindmate/MainActivity.java
+++ app/src/main/java/com/example/mindmate/MainActivity.java
@@ -1,3 +1,16 @@
 package com.example.mindmate;
-public class MainActivity {
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Accesso corretto alla ImageView logoMindMate
+        ImageView logoMindMate = findViewById(R.id.logoMindMate);
+        // Puoi ora lavorare con logoMindMate, ad esempio cambiare immagine, aggiungere listener, ecc.
+    }
 }
